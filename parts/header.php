d9499ae1c6b70118c4063b180a8294ffd6a4a3a5
Rácz <rczj@yahoo.com>
2026-03-27 18:58:06 +0100

    Add logo by whatislove

--- parts/header.php
+++ parts/header.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_SESSION['authenticated'])) {
     echo('<div class="header">
-        <a href="index.php"><h1>bustaposts</h1></a>
+        <a href="index.php"><img src="img/bustaPostss.jpg"></a><br>
     <a href="user?id=' . $_SESSION['id'] . '">@' . $_SESSION['handle'] . '</a> -
     <a href="logout.php">destroy session</a>
     <hr>');
