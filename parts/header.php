615709dfbd74cc36d7c09dd0e8119d7768bc2e4c
Rácz <rczj@yahoo.com>
2026-05-05 18:36:42 +0200

    Boobstrap 5 initial commit

--- parts/header.php
+++ parts/header.php
@@ -1,15 +1,21 @@
 <?php
 if(isset($_SESSION['authenticated'])) {
-    echo('<div class="header">
-        <a href="index.php"><img src="img/bustaPostss.jpg"></a><br>
-    <a href="user?id=' . $_SESSION['id'] . '">@' . $_SESSION['handle'] . '</a> -
-    <a href="logout.php">destroy session</a>
-    <hr>');
-    } else {
-        echo('<div class="header">
-        <a href="index.php"><h1>bustaposts</h1></a>
-        <a href="register.php">register</a> -
-        <a href="login.php">login</a>
-        <hr>');
+    echo('
+        <div class="header">
+            <a href="index.php"><img src="img/bustaPostss.jpg"></a><br>
+            <a href="user?id=' . $_SESSION['id'] . '">@' . $_SESSION['handle'] . '</a> -
+            <a href="logout.php">destroy session</a>
+        </div>
+        <hr>
+        ');
+        } else {
+            echo('
+            <div class="header">
+                <a href="index.php"><img src="img/bustaPostss.jpg"></a><br>
+                <a href="register.php">register</a> -
+                <a href="login.php">login</a>
+            </div>
+            <hr>
+            ');
     }
 ?>
